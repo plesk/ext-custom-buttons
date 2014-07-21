62b3b84049f14a07e6b585a7b39a7c87389a930f
Eugene Kazakov <ekazakov@parallels.com>
2014-07-21 10:18:52 +0700

    Add copyrights.

--- plib/resources/locales/en-US.php
+++ plib/resources/locales/en-US.php
@@ -1,5 +1,5 @@
 <?php
-
+// Copyright 1999-2014. Parallels IP Holdings GmbH. All Rights Reserved.
 $messages = [
     'commonButtonTitle' => 'Common Button',
     'commonButtonDescription' => 'Description of a common button.',
