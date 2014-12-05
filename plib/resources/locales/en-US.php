8565280ea97d7a35b6ed144734e1a8a74a3fca69
Alexei Yuzhakov <ayuzhakov@parallels.com>
2014-12-05 11:10:38 +0600

    More correct copyright notice.

--- plib/resources/locales/en-US.php
+++ plib/resources/locales/en-US.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright 1999-2014. Parallels IP Holdings GmbH. All Rights Reserved.
+// Copyright 1999-2014. Parallels IP Holdings GmbH.
 $messages = [
     'commonButtonTitle' => 'Common Button',
     'commonButtonDescription' => 'Description of a common button.',
